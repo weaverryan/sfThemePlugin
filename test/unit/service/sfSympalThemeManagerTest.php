3f31a7295d55fff9fa23ee77bb13fe6b33f77843
Ryan Weaver <ryan@thatsquality.com>
2010-04-03 19:57:05 -0500

    [1.0][test] Adding and modifying the theme manager test

--- test/unit/service/sfSympalThemeManagerTest.php
+++ test/unit/service/sfSympalThemeManagerTest.php
@@ -6,12 +6,16 @@ require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 //$configuration = ProjectConfiguration::getApplicationConfiguration('sympal', 'test', true);
 //$contexct = sfContext::createInstance($configuration)->dispatch();
 
-$t = new lime_test(24);
+$t = new lime_test(4);
 
 $themeManager = new sfSympalThemeManager($context);
 
-$themes = $sympalConfiguration->getThemes(); 
-$t->is(isset($themes['default']), true, '->getThemes() includes default theme'); 
 
-$availableThemes = $sympalConfiguration->getAvailableThemes(); 
-$t->is(isset($themes['admin']), 'admin', '->getAvailableThemes() does not include admin theme'); 
+$t->info('1 - Test getThemes(), getAvailableThemes()');
+$themes = $themeManager->getThemes(); 
+$t->is(isset($themes['unavailable_theme']), true, '->getThemes() includes unavailable_theme (but enabled) theme'); 
+$t->is(count($themes), 4, '->getThemes() should return 4 themes (3 from the plugin, 1 non-disabled from the project)');
+
+$availableThemes = $themeManager->getAvailableThemes(); 
+$t->is(isset($availableThemes['unavailable_theme']), false, '->getAvailableThemes() does not include unavailable_theme theme'); 
+$t->is(count($availableThemes), 3, '->getAvailableThemes() returns 3 themes - the 2 in the app are not available');
