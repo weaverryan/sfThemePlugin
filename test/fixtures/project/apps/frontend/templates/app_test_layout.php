6387e11989576a2db6d6d5ea3069c8e0bf69c23b
Ryan Weaver <ryan@thatsquality.com>
2010-05-01 16:42:47 -0500

    Various small adjustments to the test project itself to get it ready to work with the tests.

--- test/fixtures/project/apps/frontend/templates/app_test_layout.php
+++ test/fixtures/project/apps/frontend/templates/app_test_layout.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href="/favicon.ico" />
   </head>
   <body>
-    <h1>Test Layout</h1>
+    <h1>Application Test Layout</h1>
     <?php echo $sf_content ?>
   </body>
 </html>
