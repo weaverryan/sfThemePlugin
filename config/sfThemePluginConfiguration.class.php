f4894d6e558ae9a3c6f93e2ce5b03d73e3b811ed
Rafael Goulart <rafaelgou@gmail.com>
2010-10-01 12:31:12 -0300

    Loading Helpers  Theme and I18N automaticly

--- config/sfThemePluginConfiguration.class.php
+++ config/sfThemePluginConfiguration.class.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin configuration for the theme plugin
- * 
+ *
  * @package     sfThemePlugin
  * @subpackage  config
  * @author      Ryan Weaver <ryan@thatsquality.com>
@@ -38,7 +38,7 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
     if (sfConfig::get('app_theme_enabled', false))
     {
       $this->dispatcher->connect('context.load_factories', array($this, 'bootstrap'));
-      
+
       // Add a listener on controller.change_action - key to setting correct theme
       $this->dispatcher->connect('controller.change_action', array($this, 'listenControllerChangeAction'));
 
@@ -52,9 +52,13 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
 
       // register the web debug panel
       $this->dispatcher->connect('debug.web.load_panels', array($this, 'listenDebugWebLoadPanels'));
+
+      // loading helpers
+      $this->configuration->loadHelpers(array('Theme', 'I18N'));
+
     }
   }
-  
+
   /**
    * Listens to context.load_factories. Bootstraps the plugin
    */
@@ -62,7 +66,7 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
   {
     // store the context so we can use it for other listeners
     $this->_context = $event->getSubject();
-    
+
     // create the theme manager instance
     $this->_themeManager = sfThemeManager::createInstance($this->_context);
 
@@ -73,7 +77,7 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
   /**
    * Listens to the controller.change_action event to set the correct theme
    * when the action changes
-   * 
+   *
    * @param sfEvent $event The controller.change_action event object
    */
   public function listenControllerChangeAction(sfEvent $event)
@@ -93,7 +97,7 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
       $this->_context,
       array_keys($this->getThemeManager()->getAvailableThemes())
     );
-    
+
     // If we found a theme we should use, set it on the theme manager
     if ($theme)
     {
@@ -103,9 +107,9 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
 
   /**
    * Returns the current sfThemeManager instance for the application
-   * 
+   *
    * The theme manager is loaded automatically on context.load_factories
-   * 
+   *
    * @return sfThemeManager
    */
   public function getThemeManager()
@@ -114,14 +118,14 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
     {
       throw new sfException('No theme manager instance found');
     }
-    
+
     return $this->_themeManager;
   }
 
   /**
    * Returns the theme controller object, which is responsible for figuring
    * out what theme should be used based on a variety of variables
-   * 
+   *
    * @return sfThemeController
    */
   public function getThemeController()
@@ -130,7 +134,7 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
     {
       $this->_themeController = sfThemeController::createInstance();
     }
-    
+
     return $this->_themeController;
   }
 
@@ -143,7 +147,7 @@ class sfThemePluginConfiguration extends sfPluginConfiguration
     {
       $this->_themeToolkit = sfThemeToolkit::createInstance($this->configuration);
     }
-    
+
     return $this->_themeToolkit;
   }
 
